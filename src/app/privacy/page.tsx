424dc6679db078e598787315b4a5800642ba97d7
Faqih <e0052804@u.nus.edu>
2025-03-23 22:05:20 +1100

    Add terms of use and privacy policy pages

---
+++ src/app/privacy/page.tsx
@@ -0,0 +1,234 @@
+export default function Privacy() {
+    return (
+        <main className="bg-white py-12 px-4 md:px-8">
+            <div className="max-w-4xl mx-auto">
+                <h1 className="text-3xl font-bold text-black mb-6 font-poppins">
+                    Privacy Policy
+                </h1>
+
+                <div className="space-y-6 text-gray-700">
+                    <p>
+                        Hyan Consulting is committed to protecting your privacy
+                        and subscribes to the Singapore personal data protection
+                        principles. The following policy outlines how we will
+                        process all of your personal details. By using our
+                        services and web site - www.hyanconsulting.com and
+                        sending us any personal information, you are deemed to
+                        have read and accepted this Privacy Policy. You can
+                        access our home page and browse our site without
+                        disclosing your personal data.
+                    </p>
+
+                    <h2 className="text-2xl font-medium text-black mt-6 mb-3 font-poppins">
+                        COOKIES
+                    </h2>
+                    <p>
+                        Cookies are pieces of information that a website
+                        transfers to your computer's hard disk for record
+                        keeping purposes. Most web browsers are set to accept
+                        cookies. Hyan Consulting may use cookies to make your
+                        use of our website and services as convenient as
+                        possible. Cookies are useful to estimate our number of
+                        members, determine overall traffic patterns through our
+                        website and help us to continue improving our website.
+                        If you do not wish to receive any cookies you may set
+                        your browser to refuse cookies. This may mean you will
+                        not be able to take full advantage of the services Hyan
+                        Consulting offers.
+                    </p>
+
+                    <h2 className="text-2xl font-medium text-black mt-6 mb-3 font-poppins">
+                        COLLECTION OF INFORMATION
+                    </h2>
+                    <p>
+                        The personal details we will collect from your
+                        application includes information such as email address,
+                        name, telephone number, education, employment history,
+                        skills, motivations as well as your resume. This list,
+                        however, is not exhaustive and is open to change. For
+                        example in the course of identifying suitable employment
+                        opportunities for you we may make enquiries of referees
+                        whose names you have provided to us. This information
+                        will be added to that which we already retain. We can
+                        provide a copy of your personal information which we
+                        retain should you request it. If you request us to
+                        delete your information from our database, we will do
+                        so.
+                    </p>
+
+                    <h2 className="text-2xl font-medium text-black mt-6 mb-3 font-poppins">
+                        USE AND DISCLOSURE OF PERSONAL INFORMATION
+                    </h2>
+                    <p>
+                        In some circumstances we may collect personal and
+                        sensitive information. This personal and sensitive
+                        information will be used in connection with your actual
+                        or possible work placement as well as our assessment of
+                        your ongoing performance and prospects. As mentioned
+                        above, this information might include:
+                    </p>
+                    <ul className="list-disc pl-6 mt-2 mb-4">
+                        <li>
+                            Results of inquiries that we might make of your
+                            former employers, work colleagues, professional
+                            associations or registration body;
+                        </li>
+                        <li>Results of any competency or medical tests;</li>
+                        <li>
+                            Performance feedback (whether positive or negative);
+                        </li>
+                        <li>
+                            Additional information about you which you have
+                            provided to us.
+                        </li>
+                    </ul>
+                    <p>Your personal information may be disclosed to:</p>
+                    <ul className="list-disc pl-6 mt-2 mb-4">
+                        <li>
+                            Potential and actual employers and clients of Hyan
+                            Consulting;
+                        </li>
+                        <li>
+                            Other staff at Hyan Consulting in connection with
+                            possible employment opportunities;
+                        </li>
+                        <li>
+                            Any person or body with a lawful entitlement to
+                            obtain information.
+                        </li>
+                    </ul>
+                    <p>
+                        If you do not provide Hyan Consulting with the
+                        information we seek, we may be limited in our ability to
+                        locate suitable employment for you. Your details will
+                        not be disclosed to any person, body or organisation for
+                        any purpose other than the primary purpose of us holding
+                        your information, that being the securing of employment
+                        for you. We may retain and use information about you for
+                        research, benchmarking, studies, trend analysis and to
+                        improve and enhance our Services or develop and market
+                        new services. Any published end product will refer only
+                        to larger aggregations of individuals and will not
+                        identify you personally or include any results
+                        attributable to you.
+                    </p>
+
+                    <h2 className="text-2xl font-medium text-black mt-6 mb-3 font-poppins">
+                        CONTACTING YOU
+                    </h2>
+                    <p>
+                        We may contact you by phone, email or other address
+                        which you provide to us in order to:
+                    </p>
+                    <ul className="list-disc pl-6 mt-2 mb-4">
+                        <li>
+                            Advise you as to the progress of your application;
+                        </li>
+                        <li>
+                            Respond to a request for information or a product or
+                            service;
+                        </li>
+                        <li>
+                            Seek your permission to pass on your resume for a
+                            potential placement or employment opportunity;
+                        </li>
+                        <li>
+                            Provide you with updated information about our
+                            website, and
+                        </li>
+                        <li>
+                            Provide you with information about our products and
+                            services which you may find of use.
+                        </li>
+                    </ul>
+                    <p>
+                        If you are receiving promotional information about Hyan
+                        Consulting and do not wish to receive this information
+                        any longer, you may remove your name from our list by
+                        emailing us at contact@hyanconsulting.com (with subject
+                        title "Unsubscribe from Mailing List") and asking to be
+                        removed from our mailing list. Please allow 14 days for
+                        this request to be processed.
+                    </p>
+
+                    <h2 className="text-2xl font-medium text-black mt-6 mb-3 font-poppins">
+                        CHANGING AND DELETING YOUR INFORMATION
+                    </h2>
+                    <p>
+                        If at any time you wish to change personal information
+                        that is inaccurate or out of date, please contact us by
+                        emailing us at contact@hyanconsulting.com (with subject
+                        title "Personal Data") and we will amend this record. If
+                        you wish to have your personal information deleted,
+                        please advise us in the same manner as referred to above
+                        and we will take all reasonable steps to delete it
+                        unless we need to keep it for legal reasons.
+                    </p>
+
+                    <h2 className="text-2xl font-medium text-black mt-6 mb-3 font-poppins">
+                        STORAGE AND SECURITY OF YOUR PERSONAL INFORMATION
+                    </h2>
+                    <p>
+                        Hyan Consulting will endeavour to take all reasonable
+                        steps to keep secure any information which we hold about
+                        you, and to keep this information accurate and up to
+                        date. Your information is stored on secure servers that
+                        are protected in controlled facilities. We require our
+                        employees and data processors to respect the
+                        confidentiality of any personal information held by Hyan
+                        Consulting. Unfortunately, no data transmission over the
+                        internet can be guaranteed to be 100% secure, so we
+                        cannot give an absolute assurance that the information
+                        you provide to us will be secure at all times. Hyan
+                        Consulting will not be held responsible for events
+                        arising from unauthorised access to your personal
+                        information. Please also note that Hyan Consulting may
+                        transfer your personal information to overseas
+                        facilities for processing and storage. However, this
+                        does not change any of our commitments to safeguard your
+                        privacy. You can also play an important role in keeping
+                        your personal information secure, by maintaining the
+                        confidentiality of any password and accounts used on the
+                        Hyan Consulting website. Please notify us immediately if
+                        there is any unauthorised use of your account by any
+                        other internet user or any other breach of security.
+                    </p>
+
+                    <h2 className="text-2xl font-medium text-black mt-6 mb-3 font-poppins">
+                        FUTURE CHANGES
+                    </h2>
+                    <p>
+                        Our commitment to be a leading provider of recruitment
+                        services means that our business will continue to evolve
+                        as we introduce new services and features to our
+                        website. Because of this, from time to time, our
+                        policies will be reviewed and may be revised. Hyan
+                        Consulting reserves the right to change its privacy
+                        policy at any time and notify you by posting an updated
+                        version of the policy on its website.
+                    </p>
+
+                    <h2 className="text-2xl font-medium text-black mt-6 mb-3 font-poppins">
+                        WHAT TO DO IF YOU HAVE A PROBLEM OR A QUESTION
+                    </h2>
+                    <p>
+                        If you have any further queries relating to our Privacy
+                        Policy, or you have a problem or complaint, please
+                        contact us at:
+                    </p>
+                    <div className="mt-4">
+                        <p className="font-semibold">
+                            HYAN CONSULTING - SINGAPORE
+                        </p>
+                        <p>
+                            111 N Bridge Rd, #20-04 Peninsula Plaza, Singapore
+                            179098
+                        </p>
+                        <p>T: +65 9763 1424</p>
+                        <p>E: contact@hyanconsulting.com</p>
+                    </div>
+                </div>
+            </div>
+        </main>
+    );
+}
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -72,6 +72,17 @@ export default function Footer() {
             <div className="max-w-6xl mx-auto mt-5 pt-5 border-t border-[#80D9E2]">
                 <div className="flex flex-col md:flex-row justify-between items-center">
                     <p>© 2025 Hyan Consulting Ptd Ltd. All rights reserved.</p>
+
+                    {/* Legal Links */}
+                    <div className="flex space-x-4 mt-2 md:mt-0 text-sm">
+                        <Link href="/terms" className="hover:underline">
+                            Terms of Use
+                        </Link>
+                        <Link href="/privacy" className="hover:underline">
+                            Privacy Policy
+                        </Link>
+                    </div>
+
                     <div className="flex space-x-4 mt-4 md:mt-0">
                         <Link
                             href="#"
